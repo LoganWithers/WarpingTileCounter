60dd42026ed64c58496357596440747f789202b9
Logan Withers <withel75@uwosh.edu>
2019-04-17 22:07:43 -0500

    remove code from program.cs

--- WarpingCounter/WarpingCounter/Program.cs
+++ WarpingCounter/WarpingCounter/Program.cs
@@ -6,22 +6,13 @@
     using System.Linq;
     using System.Numerics;
 
-    using Common;
     using Common.IO;
     using Common.Models;
 
-    using Gadgets;
-    using Gadgets.DigitTop;
-    using Gadgets.ReturnAndRead.NextDigit;
-    using Gadgets.ReturnAndRead.NextRow;
-    using Gadgets.Warping;
 
     internal static class Program
     {
-
-        private const int Digits = 3;
-
-
+        
         private static void Main(string[] args)
         {
             RunCLI();
@@ -53,8 +44,6 @@
                             continue;
                         }
                   
-                        //List<Tile> tiles = seedCreator.Tiles;
-                        //AddCounterTiles(tiles, seedCreator.Construction);
                         Write($"WarpingCounter_b{baseM}_from_{input}", generator.Generate());
 
                         continue;
@@ -107,135 +96,6 @@
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine(message);
             Console.ResetColor();
-        }
-
-
-        private static void AddCounterTiles(List<Tile> tiles, ConstructionValues construction)
-        {
-            var inMSR   = construction.DigitsInMSR;
-            var counter = new ReaderFactory(construction.L, construction.BitsRequiredForBaseM, construction.BaseM, construction.DigitsInMSR);
-            tiles.AddRange(counter.Readers.SelectMany(reader => reader.Tiles));
-            tiles.AddRange(CreateReturnAndRead(construction.L, construction.DigitRegions, inMSR));
-            tiles.AddRange(CreateDigitTops(construction.L, inMSR));
-
-            Console.WriteLine($"Bits Per Counter Digit: {construction.BitsRequiredForBaseM}");
-            Console.WriteLine($"Actual Bits Per Digit:  {construction.L}");
-
-            List<string> fullSizeDigits = counter.digitsThatCanBeRead.Where(d => d.Length == construction.L)
-                                                 .ToList();
-
-            Console.WriteLine($"Full Digits: {fullSizeDigits.Count}");
-
-            foreach (var binaryString in fullSizeDigits)
-            {
-                tiles.AddRange(CreateWarpUnits(binaryString, construction.DigitsInMSR));
-                tiles.AddRange(CreateWriters(binaryString, construction.DigitsInMSR));
-            }
-        }
-
-
-        private static IEnumerable<Tile> CreateDigitTops(int bitsPerDigit, int digitsInMSR)
-        {
-            var results = new List<Tile>();
-
-            for (var i = 1; i <= Digits; i++)
-            {
-                results.AddRange(new DigitTopDefault(true,  i, bitsPerDigit).Tiles);
-                results.AddRange(new DigitTopDefault(false, i, bitsPerDigit).Tiles);
-            }
-
-            foreach (var carry in new[] {true, false})
-            {
-                switch (digitsInMSR)
-                {
-                    case 1:
-
-                        break;
-
-                    case 2:
-
-                        results.AddRange(new DigitTopDigit2Case2(carry, bitsPerDigit).Tiles);
-                        results.AddRange(new DigitTopDigit1Case2(carry, bitsPerDigit).Tiles);
-
-                        break;
-
-                    case 3:
-                        results.AddRange(new DigitTopDigit3Case3(carry, bitsPerDigit).Tiles);
-
-                        break;
-                }
-            }
-
-            return results;
-        }
-
-
-        private static IEnumerable<Tile> CreateReturnAndRead(int bitsPerDigit, int regions, int digitsInMSR)
-        {
-            var results = new List<Tile>();
-
-            foreach (var carry in new[] {true, false})
-            {
-                var returnD1ReadD2 = new ReturnDigit1ReadDigit2(carry, bitsPerDigit);
-                var returnD2ReadD3 = new ReturnDigit2ReadDigit3(carry, bitsPerDigit);
-                var returnD3ReadD1 = new ReturnDigit3ReadDigit1(carry, bitsPerDigit);
-
-                results.AddRange(returnD3ReadD1.Tiles);
-                results.AddRange(returnD1ReadD2.Tiles);
-                results.AddRange(returnD2ReadD3.Tiles);
-
-                switch (digitsInMSR)
-                {
-                    case 1:
-                        var returnDigit1ReadNextRow = new ReturnDigit1ReadNextRow(carry, bitsPerDigit, regions);
-                        results.AddRange(returnDigit1ReadNextRow.Tiles);
-
-                        break;
-                    case 2:
-                        var returnDigit2ReadNextRow     = new ReturnDigit2ReadNextRow(carry, bitsPerDigit, regions);
-                        var returnDigit1ReadDigit2Case2 = new ReturnDigit1ReadDigit2Case2(carry, bitsPerDigit);
-                        results.AddRange(returnDigit1ReadDigit2Case2.Tiles);
-                        results.AddRange(returnDigit2ReadNextRow.Tiles);
-
-                        break;
-                    case 3:
-                        var returnDigit3ReadNextRow = new ReturnDigit3ReadNextRow(carry, bitsPerDigit, regions);
-                        results.AddRange(returnDigit3ReadNextRow.Tiles);
-                        results.AddRange(new DigitTopDigit3Case3(carry, bitsPerDigit).Tiles);
-
-                        break;
-                }
-            }
-
-            return results;
-        }
-
-
-        private static IEnumerable<Tile> CreateWriters(string bits, int digitsInMSR)
-        {
-            var results = new List<Tile>();
-
-            for (var i = 1; i <= Digits; i++)
-            {
-                results.AddRange(new BinaryWriter(bits, true,  i, digitsInMSR).Tiles);
-                results.AddRange(new BinaryWriter(bits, false, i, digitsInMSR).Tiles);
-            }
-
-            return results;
-        }
-
-
-        private static IEnumerable<Tile> CreateWarpUnits(string bits, int digitsInMSR)
-        {
-            var results = new List<Tile>();
-
-            for (var i = 1; i <= Digits; i++)
-            {
-                results.AddRange(new WarpUnit(bits, i, true,  digitsInMSR).Tiles);
-                results.AddRange(new WarpUnit(bits, i, false, digitsInMSR).Tiles);
-            }
-
-            return results;
         }
 
     }
